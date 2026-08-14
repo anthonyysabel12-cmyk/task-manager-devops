73c2ce33e2cbce8eb5b733215fb8a1df236ec5c3
Anderson <anthonyysabel12@gmail.com>
2026-08-14 12:56:13 -0400

    Corregir creacion de base de datos en CI

--- src/database.js
+++ src/database.js
@@ -1,7 +1,14 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const fs = require('fs');
 
-const dbPath = path.join(__dirname, '../data/database.sqlite');
+const dataDir = path.join(__dirname, '../data');
+const dbPath = path.join(dataDir, 'database.sqlite');
+
+// Crear la carpeta data si no existe
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir, { recursive: true });
+}
 
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
